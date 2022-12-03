0a9d0aae34986a562b57720ac64ecf95e1a252f5
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2022-12-03 11:38:29 +0100

    Day2 part 2

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -8,7 +8,47 @@ public class Day2 {
         File myObj = new File("src/main/inputDay2.txt");
         List<int[]> playerHands = readShapes(myObj);
 
-        var t = play(playerHands);
+        var t = play2(playerHands);
+    }
+
+    private static int play2(List<int[]> playerHands){
+        int myScore = 0;
+        int oppScore = 0;
+
+        for (int[] playerHand: playerHands) {
+            int oppMove = playerHand[0];
+            int myMove = playerHand[1];
+
+            //Should lose
+            if(myMove == 1){
+                if (oppMove == 1){
+                    myScore += 3;
+                } else if(oppMove == 2){
+                    myScore += 1;
+                } else {
+                    myScore += 2;
+                }
+                //Draw
+            } else if (myMove == 2) {
+                if (oppMove == 1){
+                    myScore += 1+3;
+                } else if(oppMove == 2){
+                    myScore += 2+3;
+                } else {
+                    myScore += 3+3;
+                }
+            } else if(myMove == 3){
+                if (oppMove == 1){
+                    myScore += 2+6;
+                } else if(oppMove == 2){
+                    myScore += 3+6;
+                } else {
+                    myScore += 1+6;
+                }            }
+
+        }
+
+        return myScore;
     }
 
     private static int play(List<int[]> playerHands){
@@ -23,17 +63,17 @@ public class Day2 {
                 myScore += myMove + 3;
                 oppScore += oppMove + 3;
             //Rock -- Paper
-            } else if(oppMove == 1 && myMove == 2){
-                oppScore += oppMove;
-                myScore += myMove + 6;
+            } else if(oppMove == 2 && myMove == 2){
+                oppScore += oppMove + 3;
+                myScore += myMove + 3;
             } else if(oppMove == 2 && myMove == 1){
                 oppScore += oppMove + 6;
                 myScore += myMove;
             }
             //Rock -- Scissors
-            else if (oppMove == 1 && myMove == 3) {
-                oppScore += oppMove + 6;
-                myScore += myMove;
+            else if (oppMove == 2 && myMove == 3) {
+                oppScore += oppMove;
+                myScore += myMove+6;
             } else if (oppMove == 3 && myMove == 1) {
                 oppScore += oppMove;
                 myScore += myMove + 6;
@@ -42,9 +82,9 @@ public class Day2 {
             else if (oppMove == 2 && myMove == 3){
                 oppScore += oppMove;
                 myScore += myMove + 6;
-            } else if (oppMove == 3 && myMove == 2){
-                oppScore += oppMove + 6;
-                myScore += myMove;
+            } else if (oppMove == 2 && myMove == 2){
+                oppScore += oppMove + 3;
+                myScore += myMove + 3;
             }
 
         }
