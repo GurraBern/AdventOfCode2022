20bbdd237c90452e300d34ebfd47392275c4a4f0
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2022-12-06 16:53:42 +0100

    Day 6 aoc Part1

--- src/main/java/Day6.java
+++ src/main/java/Day6.java
@@ -1,32 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day6 {
     public static void main(String[] args) throws FileNotFoundException {
-        File myObj = new File("src/main/inputDay3.txt");
+        File myObj = new File("src/main/inputDay6.txt");
         Scanner myReader = new Scanner(myObj);
 
         while (myReader.hasNextLine()) {
             var inputString = myReader.nextLine();
 
-            //add 4 items to add set
-            //if equal to 4 we have the signal start
-
+            int index = 0;
             for(int i=0; i<inputString.length()-1; i++){
-
+                Set<Character> charSet = new HashSet<>();
+                var split = inputString.substring(i,i+4);
+                for (var c : split.toCharArray()){
+                    charSet.add(c);
+                }
+                if(charSet.size() >= 4){
+                    index = i+4;
+                    break;
+                }
             }
 
-            /*List<Character> charList = new ArrayList<>();
-            for(char c : inputString.toCharArray()){
-                if(charList.size() < 4){
-                    charList.add(c);
-                } else {
-                    charList.clear();
-                }
-            }*/
+            var res = index;
         }
     }
 
