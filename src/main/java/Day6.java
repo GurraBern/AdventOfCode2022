ad4b5a16ffb19a57be31f343e25c0dc63efca986
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2022-12-06 17:08:32 +0100

    Day 6 aoc Part2

--- src/main/java/Day6.java
+++ src/main/java/Day6.java
@@ -7,24 +7,28 @@ public class Day6 {
         File myObj = new File("src/main/inputDay6.txt");
         Scanner myReader = new Scanner(myObj);
 
+        var part1 = 0;
+        var part2 = 0;
         while (myReader.hasNextLine()) {
             var inputString = myReader.nextLine();
-
-            int index = 0;
-            for(int i=0; i<inputString.length()-1; i++){
-                Set<Character> charSet = new HashSet<>();
-                var split = inputString.substring(i,i+4);
-                for (var c : split.toCharArray()){
-                    charSet.add(c);
-                }
-                if(charSet.size() >= 4){
-                    index = i+4;
-                    break;
-                }
-            }
-
-            var res = index;
+            part1 = checkForMessage(inputString, 4);
+            part2 = checkForMessage(inputString, 14);
         }
+
+        System.out.println("Part 1 is: " + part1 + ", Part 2 is: " + part2);
     }
 
+    private static int checkForMessage(String inputString, int size){
+        for(int i=0; i<inputString.length()-1; i++){
+            Set<Character> charSet = new HashSet<>();
+            var split = inputString.substring(i,i+size);
+            for (var c : split.toCharArray()){
+                charSet.add(c);
+            }
+            if(charSet.size() >= size){
+                return i+size;
+            }
+        }
+        return -1;
+    }
 }
