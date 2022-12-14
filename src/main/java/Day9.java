6be261d9e9707314ea3a120dc2f92f5b8ca19cdf
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2022-12-14 01:01:51 +0100

    Day9 part 2 solution

--- src/main/java/Day9.java
+++ src/main/java/Day9.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.*;
 
 public class Day9 {
-
-
     private static List<List<String>> grid = new ArrayList<>();
     private static Set<Point> markedTiles = new HashSet<>();
     private static int tailLength = 9;
@@ -15,8 +13,6 @@ public class Day9 {
     private static TailSegment head = new TailSegment("H", start);
 
     private static List<TailSegment> tails = new ArrayList<>();
-
-
 
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -123,4 +119,53 @@ public class Day9 {
         }
     }
 
+    static class TailSegment {
+
+
+        private String name;
+        private Point position = new Point();
+        private TailSegment parent;
+
+        public TailSegment(String name, TailSegment parent){
+            this.name = name;
+            this.parent = parent;
+            this.position = new Point(parent.position);
+        }
+
+        public TailSegment(String name, Point position){
+            this.name = name;
+            this.position = position;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public void move(int x, int y){
+            position.setLocation(x,y);
+        }
+
+        public Point getPosition() {
+            return position;
+        }
+
+        public Point getParentPosition() {
+            return new Point(parent.position);
+        }
+
+        public TailSegment getParent() {
+            return parent;
+        }
+
+        public int getX(){
+            return position.x;
+        }
+
+        public int getY(){
+            return position.y;
+        }
+    }
+
+
 }
+
